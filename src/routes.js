2aad4c758135b6dc711ea60a400ac656306c18d5
LeuAlmeida <leo@webid.net.br>
2019-11-04 19:02:16 -0300

    Improving routes

--- src/routes.js
+++ src/routes.js
@@ -8,4 +8,6 @@ const routes = new Router();
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
+routes.put('/users', UserController.update);
+
 export default routes;
