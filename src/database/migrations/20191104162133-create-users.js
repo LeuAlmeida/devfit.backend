2dbe09f7de195928e968a49199bf7fde3717a0d3
LeuAlmeida <leo@webid.net.br>
2019-11-04 13:24:13 -0300

    Users migration schema

--- src/database/migrations/20191104162133-create-users.js
+++ src/database/migrations/20191104162133-create-users.js
@@ -3,7 +3,25 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
       return queryInterface.createTable('users', {
-        id: Sequelize.INTEGER
+        id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        name: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        email: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          unique: true,
+        },
+        password_hash: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        }
       });
   },
 
