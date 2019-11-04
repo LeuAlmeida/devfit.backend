9dae046d02c7a735edae6bd20dbd9cc827990ec9
LeuAlmeida <leo@webid.net.br>
2019-11-04 14:41:30 -0300

    Improving routes using controllers

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Leonardo Almeida',
-    email: 'leo@webid.net.br',
-    password_hash: '12345678',
-  })
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
