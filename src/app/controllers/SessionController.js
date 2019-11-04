2d5dbe62e405959ee69f73f7df47ec82bd2c0374
LeuAlmeida <leo@webid.net.br>
2019-11-04 18:36:05 -0300

    Initial structure and user verifier

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,7 +1,17 @@
+import jwt from 'jsonwebtoken';
+
 import User from '../models/User';
 
 class SessionController {
   async store(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email }});
+
+    if (!user) {
+      return res.status(401).json({ error: 'User not found.' });
+    }
+
 
   }
 }
