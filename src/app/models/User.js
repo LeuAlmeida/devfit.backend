11250f1b6af5f40f2e9b31996fa5fc2e2b6af858
LeuAlmeida <leo@webid.net.br>
2019-11-04 14:58:29 -0300

    Create bcrypt hooks

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -6,12 +7,17 @@ class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        passowrd: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
       },
       {
         sequelize,
       }
     );
+
+    this.addHook('beforeSave', () => {
+
+    });
   }
 }
 
