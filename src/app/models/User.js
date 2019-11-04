89698c4c5e1d90b3a127ac7f5e3c1d2d9ee7874f
LeuAlmeida <leo@webid.net.br>
2019-11-04 15:00:52 -0300

    Improving User password using bcrypt and hooks

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -15,9 +15,13 @@ class User extends Model {
       }
     );
 
-    this.addHook('beforeSave', () => {
-
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
     });
+
+    return this;
   }
 }
 
