0c60a2721e6e515c8bbe4d1bd1d01b98be90d9ce
LeuAlmeida <leo@webid.net.br>
2019-11-04 14:54:12 -0300

    Improving user controller with middleware verifier

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,9 +2,19 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    const user = await User.create(req.body);
+    const userExists = await User.findOne({ where: { email: req.body.email }});
 
-    return res.json(user);
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists.' });
+    }
+
+    const { id, name, email } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email
+    });
   }
 }
 
