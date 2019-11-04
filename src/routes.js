c506779ca582ff8c1c0933c5109f509278675f4c
LeuAlmeida <leo@webid.net.br>
2019-11-04 14:34:17 -0300

    Create user controller

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello world' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Leonardo Almeida',
+    email: 'leo@webid.net.br',
+    password_hash: '12345678',
+  })
+
+  return res.json(user);
 });
 
 export default routes;
