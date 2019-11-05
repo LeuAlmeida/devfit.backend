f146d711441e6301345586810eb8d3af9bd5354b
LeuAlmeida <leo@webid.net.br>
2019-11-05 13:28:41 -0300

    Finishing store method to create new Student

--- src/app/controllers/StudentController.js
+++ src/app/controllers/StudentController.js
@@ -2,7 +2,17 @@ import Student from '../models/Student';
 
 class StudentController {
   async store(req, res) {
-    const { id, name, email, age, weight, height } = await Student.create(req.body);
+    const studentExists = await Student.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (studentExists) {
+      return res.status(400).json({ error: 'Student already exists.' });
+    }
+
+    const { id, name, email, age, weight, height } = await Student.create(
+      req.body
+    );
 
     return res.json({
       id,
@@ -11,7 +21,7 @@ class StudentController {
       age,
       weight,
       height,
-     })
+    });
   }
 }
 
