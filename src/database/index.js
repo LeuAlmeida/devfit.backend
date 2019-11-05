d61ca96902a26f4c3036ec71e6dadd990f024178
LeuAlmeida <leo@webid.net.br>
2019-11-05 13:12:35 -0300

    Improving index database file with Students model

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,11 @@
 import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
+import Student from '../app/models/Student';
 
 import databaseConfig from '../config/database';
 
-const models = [User];
+const models = [User, Student];
 
 class Database {
   constructor() {
