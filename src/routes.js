f2883bc5dec601e90daa40c5c9e4809ed2a5af28
LeuAlmeida <leo@webid.net.br>
2019-11-05 13:14:22 -0300

    Update routes with Student route

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import StudentController from './app/controllers/StudentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -13,5 +14,7 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.post('/students', StudentController.store);
 
 export default routes;
