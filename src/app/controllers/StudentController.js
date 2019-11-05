5a14d0043bcf3e4ffd291291ac0920553b8f3199
LeuAlmeida <leo@webid.net.br>
2019-11-05 15:10:05 -0300

    Finishing the yup validations on store method

--- src/app/controllers/StudentController.js
+++ src/app/controllers/StudentController.js
@@ -1,7 +1,21 @@
+import * as Yup from 'yup';
+
 import Student from '../models/Student';
 
 class StudentController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      age: Yup.number().required(),
+      weight: Yup.number().required(),
+      height: Yup.number().required(),
+    })
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' })
+    }
+
     const studentExists = await Student.findOne({
       where: { email: req.body.email },
     });
