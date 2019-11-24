217dfae7d6225649d0749c08fef015280ef6960a
Léu Almeida <leo@webid.net.br>
2019-11-24 20:59:40 -0300

    Improving indent using ESLint

--- src/app/models/Student.js
+++ src/app/models/Student.js
@@ -2,19 +2,21 @@ import Sequelize, { Model } from 'sequelize';
 
 class Student extends Model {
   static init(sequelize) {
-    super.init({
-      name: Sequelize.STRING,
-      email: Sequelize.STRING,
-      age: Sequelize.INTEGER,
-      weight: Sequelize.FLOAT,
-      height: Sequelize.FLOAT,
-    },{
-      sequelize
-    });
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        age: Sequelize.INTEGER,
+        weight: Sequelize.FLOAT,
+        height: Sequelize.FLOAT,
+      },
+      {
+        sequelize,
+      }
+    );
 
     return this;
   }
-
 }
 
 export default Student;
--- src/app/models/User.js
+++ src/app/models/User.js
@@ -15,7 +15,7 @@ class User extends Model {
       }
     );
 
-    this.addHook('beforeSave', async (user) => {
+    this.addHook('beforeSave', async user => {
       if (user.password) {
         user.password_hash = await bcrypt.hash(user.password, 8);
       }
