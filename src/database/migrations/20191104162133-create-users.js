0510ccd8f37b5e6b5dfbed7c0af4eaff017a1dde
Léu Almeida <leo@webid.net.br>
2019-11-24 20:53:42 -0300

    Improving using ESLint

--- src/database/migrations/20191104162133-create-users.js
+++ src/database/migrations/20191104162133-create-users.js
@@ -1,39 +1,37 @@
-'use strict';
-
 module.exports = {
   up: (queryInterface, Sequelize) => {
-      return queryInterface.createTable('users', {
-        id: {
-          type: Sequelize.INTEGER,
-          allowNull: false,
-          autoIncrement: true,
-          primaryKey: true,
-        },
-        name: {
-          type: Sequelize.STRING,
-          allowNull: false,
-        },
-        email: {
-          type: Sequelize.STRING,
-          allowNull: false,
-          unique: true,
-        },
-        password_hash: {
-          type: Sequelize.STRING,
-          allowNull: false,
-        },
-        created_at: {
-          type: Sequelize.DATE,
-          allowNull: false,
-        },
-        updated_at: {
-          type: Sequelize.DATE,
-          allowNull: false,
-        },
-      });
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
   },
 
-  down: (queryInterface) => {
-      return queryInterface.dropTable('users');
-  }
+  down: queryInterface => {
+    return queryInterface.dropTable('users');
+  },
 };
