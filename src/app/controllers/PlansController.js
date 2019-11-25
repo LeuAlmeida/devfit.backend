4c9f6b878c67a4814d3ee8fb5154351daca89b39
Léu Almeida <leo@webid.net.br>
2019-11-24 21:29:51 -0300

    Improving PlansController

--- src/app/controllers/PlansController.js
+++ src/app/controllers/PlansController.js
@@ -1,3 +1,11 @@
-class PlansController {}
+import Plans from '../models/Plans';
+
+class PlansController {
+  async store(req, res) {
+    const { title, duration, price } = Plans.create(req.body);
+
+    return res.json(title, duration, price);
+  }
+}
 
 export default new PlansController();
