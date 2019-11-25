1c94cf1f5810255c7b9983617a2dc0461e6882b9
LeuAlmeida <leo@webid.net.br>
2019-11-25 10:32:00 -0300

    Finishing the enrollments migration

--- src/database/migrations/20191125132821-create-enrollments.js
+++ src/database/migrations/20191125132821-create-enrollments.js
@@ -1,9 +1,38 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    return queryInterface.createTable('enrollments', {
+      student_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      plan_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      start_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      end_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
   },
 
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('users');
+  down: queryInterface => {
+    return queryInterface.dropTable('enrollments');
   },
 };
