ea9f2a6780489123aa9d5ab84a8a82ef9ef62ecd
LeuAlmeida <leo@webid.net.br>
2019-11-25 12:34:55 -0300

    Improving Enrollments model

--- src/app/models/Enrollments.js
+++ src/app/models/Enrollments.js
@@ -16,8 +16,11 @@ class Enrollments extends Model {
   }
 
   static associate(models) {
-    this.belongsTo(models.Student, { foreignKey: 'student_id', as: 'student' });
-    this.belongsTo(models.Plan, { foreignKey: 'plan_id' });
+    this.belongsTo(models.Student, {
+      foreignKey: 'student_id',
+      as: 'student',
+    });
+    this.belongsTo(models.Plans, { foreignKey: 'plan_id', as: 'plan' });
   }
 }
 
