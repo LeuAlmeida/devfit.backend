cf08d38334519f7a2b75bacf8b68debbbeaaf8e9
Léu Almeida <leo@webid.net.br>
2019-11-24 21:39:05 -0300

    Incrementing the store method to Plans

--- src/app/controllers/PlansController.js
+++ src/app/controllers/PlansController.js
@@ -1,10 +1,33 @@
+import * as Yup from 'yup';
 import Plans from '../models/Plans';
 
 class PlansController {
   async store(req, res) {
-    const { title, duration, price } = Plans.create(req.body);
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      duration: Yup.number().required(),
+      price: Yup.number().required(),
+    });
 
-    return res.json(title, duration, price);
+    if (!(await schema.isValid(req.body))) {
+      return res.status(401).json({ error: 'Validation fails.' });
+    }
+
+    const planDurationExists = await Plans.findOne({
+      where: { duration: req.body.duration },
+    });
+
+    if (planDurationExists) {
+      return res.status(400).json({ error: 'This plan already exists.' });
+    }
+
+    const { title, duration, price } = await Plans.create(req.body);
+
+    return res.json({
+      title,
+      duration,
+      price,
+    });
   }
 }
 
