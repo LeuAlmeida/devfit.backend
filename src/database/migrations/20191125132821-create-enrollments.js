ce5abe70f5e5d0d50401de762208856333b60919
LeuAlmeida <leo@webid.net.br>
2019-11-25 11:16:10 -0300

    Improving enrollments migration with ID

--- src/database/migrations/20191125132821-create-enrollments.js
+++ src/database/migrations/20191125132821-create-enrollments.js
@@ -1,6 +1,12 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('enrollments', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
       student_id: {
         type: Sequelize.INTEGER,
         allowNull: false,
