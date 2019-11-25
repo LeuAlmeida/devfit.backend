1861caeabb8995c162eb49d1531496769d337f27
LeuAlmeida <leo@webid.net.br>
2019-11-25 19:14:55 -0300

    Improving Checkin Controller

--- src/app/controllers/CheckinController.js
+++ src/app/controllers/CheckinController.js
@@ -1,5 +1,10 @@
 import Checkin from '../models/Checkin';
+import Enrollment from '../models/Enrollment';
 
-class CheckinController {}
+class CheckinController {
+  async store(req, res) {
+    return res.json();
+  }
+}
 
 export default new CheckinController();
