c2f243a2205c1eddc0466bc7645ec5afb2790d26
Léu Almeida <leo@webid.net.br>
2019-11-24 21:29:39 -0300

    Including Plans model

--- src/database/index.js
+++ src/database/index.js
@@ -2,10 +2,11 @@ import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
 import Student from '../app/models/Student';
+import Plans from '../app/models/Plans';
 
 import databaseConfig from '../config/database';
 
-const models = [User, Student];
+const models = [User, Student, Plans];
 
 class Database {
   constructor() {
