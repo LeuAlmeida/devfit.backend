62636893215a68f748169caccf36be1e0e4b0d6b
LeuAlmeida <leo@webid.net.br>
2019-11-25 20:08:00 -0300

    Improving Checkin Controller

--- src/app/controllers/CheckinController.js
+++ src/app/controllers/CheckinController.js
@@ -1,4 +1,4 @@
-import { eachDayOfInterval, addDays, startOfWeek } from 'date-fns';
+import { eachDayOfInterval, addDays, startOfWeek, isBefore } from 'date-fns';
 
 import Checkin from '../models/Checkin';
 import Enrollment from '../models/Enrollment';
@@ -7,71 +7,35 @@ import formatDate from '../utils/formatDate';
 
 class CheckinController {
   async store(req, res) {
-    /**
-     * Verify is the enrollment exists
-     */
-    const enrollmentExists = await Enrollment.findOne({
-      where: { student_id: req.params.id },
+    const student_id = req.params.id;
+
+    const today = new Date();
+    today.setDate(today.getDate() - 7);
+
+    const checkins = await Checkin.findAll({ where: { student_id } });
+
+    let count = 0;
+    checkins.forEach(checkin => {
+      if (isBefore(today, checkin.createdAt)) {
+        count++;
+      }
     });
-
-    if (!enrollmentExists) {
-      return res.status(400).json({ error: 'Student is not enrolled.' });
-    }
-
-    const student = await Checkin.findAll({
-      where: { student_id: req.params.id },
-    });
-
-    /**
-     * Define days of weeks
-     */
-    const dayOfWeek = eachDayOfInterval({
-      start: startOfWeek(new Date().getTime(), { weekStartsOn: 1 }),
-      end: startOfWeek(addDays(new Date().getTime(), 6)),
-    });
-
-    /**
-     * Define limit of checkins
-     */
-    const limitCheckin = dayOfWeek
-      .map(day => {
-        const listDay = formatDate(day);
-
-        const checkDate = student.find(
-          s => formatDate(s.createdAt) === listDay
-        );
-
-        return checkDate;
-      })
-      .filter(checked => checked != null);
-
-    if (limitCheckin.length === 5) {
-      return res
+    if (count >= 5) {
+      res
         .status(400)
-        .json({ error: 'This student has exceeded the limit of 5 checkins.' });
+        .json({ error: 'No more than 5 checkins for the last 7 days' });
     }
 
-    const compareDate = formatDate(new Date());
+    const checkin = await Checkin.create({ student_id });
 
-    const checkinDay = student.find(
-      check => formatDate(check.createdAt) === compareDate
-    );
-
-    if (checkinDay) {
-      return res
-        .status(400)
-        .json({ error: 'This student already checked in today.' });
-    }
-
-    const createCheckin = await Checkin.create({
-      student_id: req.params.id,
-    });
-
-    return res.json(createCheckin);
+    return res.json(checkin);
   }
 
   async index(req, res) {
-    return res.json();
+    const checkins = await Checkin.findAll({
+      student: req.params.id,
+    });
+    return res.json(checkins);
   }
 }
 
