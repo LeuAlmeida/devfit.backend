160e1d3b1b4c929d98e7f96eba6fe9e66a246ee4
Léu Almeida <leo@webid.net.br>
2019-11-24 21:04:25 -0300

    Update Plans model

---
+++ src/app/models/Plans.js
@@ -0,0 +1,20 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Plans extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        title: Sequelize.STRING,
+        duration: Sequelize.INTEGER,
+        price: Sequelize.INTEGER,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+}
+
+export default Plans;
