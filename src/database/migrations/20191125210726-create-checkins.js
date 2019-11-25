e1477e0048e168296bd55d9a28cc80b9cb7ce044
LeuAlmeida <leo@webid.net.br>
2019-11-25 18:09:39 -0300

    Improving checkins migration

--- src/database/migrations/20191125210726-create-checkins.js
+++ src/database/migrations/20191125210726-create-checkins.js
@@ -1,6 +1,27 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('checkins', { id: Sequelize.INTEGER });
+    return queryInterface.createTable('checkins', {
+      id: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      student_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'student', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
   },
 
   down: queryInterface => {
