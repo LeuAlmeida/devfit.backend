27ffc0a19e71a1985197fd4fb336c1887e919b67
LeuAlmeida <leo@webid.net.br>
2019-11-25 10:43:03 -0300

    Create enrollments model

--- src/app/models/Enrollments.js
+++ src/app/models/Enrollments.js
@@ -1,5 +1,25 @@
 import Sequelize, { Model } from 'sequelize';
 
-class Enrollments extends Model {}
+class Enrollments extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        started_date: Sequelize.DATE,
+        end_date: Sequelize.DATE,
+        price: Sequelize.FLOAT,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.Student, { foreignKey: 'student_id', as: 'student' });
+    this.belongsTo(models.Plans, { foreignKey: 'plan_id', as: 'plan' });
+  }
+}
 
 export default Enrollments;
