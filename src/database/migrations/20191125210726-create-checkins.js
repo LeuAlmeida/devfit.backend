1b2ac757ff51074a1f0e2cbe2d87237ceb0d8b89
LeuAlmeida <leo@webid.net.br>
2019-11-25 18:08:09 -0300

    Improving checkins migration

--- src/database/migrations/20191125210726-create-checkins.js
+++ src/database/migrations/20191125210726-create-checkins.js
@@ -1,23 +1,9 @@
-'use strict';
-
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    return queryInterface.createTable('checkins', { id: Sequelize.INTEGER });
   },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
-  }
+  down: queryInterface => {
+    return queryInterface.dropTable('checkins');
+  },
 };
