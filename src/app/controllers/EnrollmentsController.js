e03caff94b730cd7b9e76eb3578ad23333aadd03
LeuAlmeida <leo@webid.net.br>
2019-11-25 10:46:04 -0300

    Initial structure for enrollments controller

---
+++ src/app/controllers/EnrollmentsController.js
@@ -0,0 +1,9 @@
+import Enrollments from '../models/Enrollments';
+
+class EnrollmentsController {
+  async store(req, res) {
+    return res.json();
+  }
+}
+
+export default new EnrollmentsController();
