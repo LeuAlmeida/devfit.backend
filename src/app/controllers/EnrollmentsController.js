4316d2b38f5a61efe4751fd3acc3ee166a8503be
LeuAlmeida <leo@webid.net.br>
2019-11-25 11:16:20 -0300

    Improving enrollments controller

--- src/app/controllers/EnrollmentsController.js
+++ src/app/controllers/EnrollmentsController.js
@@ -1,9 +1,50 @@
 import Enrollments from '../models/Enrollments';
+import Plans from '../models/Plans';
+import Student from '../models/Student';
 
 class EnrollmentsController {
   async store(req, res) {
-    return res.json();
+    const { student_id, plan_id, start_date } = req.body;
+
+    // Search for an exist student
+    const student = await Student.findByPk(student_id);
+
+    if (!student) {
+      return res
+        .status(400)
+        .json({ error: 'Student does not exists in our database.' });
+    }
+
+    // Search for an exist plan
+    const plan = await Plans.findByPk(plan_id);
+
+    if (!plan) {
+      return res.status(400).json({ error: 'This plan does not exists.' });
+    }
+
+    const { price } = plan;
+
+    // Need to fix
+    const { end_date } = req.body;
+
+    const enrollment = await Enrollments.create({
+      student,
+      plan,
+      start_date,
+      end_date,
+      price,
+    });
+
+    return res.json(enrollment);
   }
 }
 
 export default new EnrollmentsController();
+
+/**
+ * [X] student_id
+ * [X] plan_id
+ * [X] start_date
+ * [X] end_date
+ * [X] price
+ */
