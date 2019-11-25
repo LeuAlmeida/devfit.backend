2f51c3250dae521262c103b9820e94ef09d9e1bb
LeuAlmeida <leo@webid.net.br>
2019-11-25 13:02:07 -0300

    Mail settings

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: '	smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'e7319c852647bd',
+    pass: '128efaea5c99e6',
+  },
+  default: {
+    from: 'Equipe DevFit <noreply@webid.net.br>',
+  },
+};
