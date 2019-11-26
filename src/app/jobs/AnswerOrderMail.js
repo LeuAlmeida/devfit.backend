224b2e44bf698ee308ec935272b1f97e85a6b25a
LeuAlmeida <leo@webid.net.br>
2019-11-26 14:19:21 -0300

    Improving AnswerOrder with images

--- src/app/jobs/AnswerOrderMail.js
+++ src/app/jobs/AnswerOrderMail.js
@@ -1,7 +1,11 @@
-// import { parseISO, format } from 'date-fns';
-// import pt from 'date-fns/locale/pt';
+import { parseISO, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
+import { resolve } from 'path';
 
 import Mail from '../../lib/Mail';
+
+const folder = resolve(__dirname, '..', 'views', 'emails', 'images');
 
 class AnswerOrderMail {
   get key() {
@@ -20,7 +24,31 @@ class AnswerOrderMail {
         yourQuestion: order.question,
         ourAnswer: answer,
         orderId: order.id,
+        answerDate: format(
+          parseISO(order.answer_at),
+          "dd' de 'MMMM' de 'yyyy",
+          {
+            locale: pt,
+          }
+        ),
       },
+      attachments: [
+        {
+          filename: 'logo.png',
+          path: `${folder}/logo.png`,
+          cid: 'logo',
+        },
+        {
+          filename: 'element1.png',
+          path: `${folder}/element1.png`,
+          cid: 'element1',
+        },
+        {
+          filename: 'footer_image.png',
+          path: `${folder}/footer_image.png`,
+          cid: 'footer_image',
+        },
+      ],
     });
   }
 }
