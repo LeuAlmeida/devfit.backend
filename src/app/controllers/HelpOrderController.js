81e4454f3fdccfbdf4e7fddca4529c5b3e2c5926
LeuAlmeida <leo@webid.net.br>
2019-11-26 11:30:41 -0300

    Improving HelpOrder controller

---
+++ src/app/controllers/HelpOrderController.js
@@ -0,0 +1,45 @@
+import * as Yup from 'yup';
+
+import Student from '../models/Student';
+import HelpOrder from '../models/HelpOrder';
+
+class HelpOrderController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      question: Yup.string()
+        .required()
+        .max(360),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(401).json({ error: 'Validation fails. ' });
+    }
+
+    const student = await Student.findOne({
+      where: { id: req.params.id },
+    });
+
+    if (!student) {
+      return res.status(400).json({ error: 'Student does not exists.' });
+    }
+
+    const student_id = student.id;
+
+    const { question } = req.body;
+
+    const createdAt = new Date();
+
+    await HelpOrder.create({
+      student_id,
+      question,
+    });
+
+    return res.json({
+      student_id,
+      question,
+      createdAt,
+    });
+  }
+}
+
+export default new HelpOrderController();
