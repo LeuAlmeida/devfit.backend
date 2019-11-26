472b21feef692c31c99e62ddb8c74060d5b102da
LeuAlmeida <leo@webid.net.br>
2019-11-26 10:57:29 -0300

    Improving help orders migration

--- src/database/migrations/20191126135206-create-help-orders.js
+++ src/database/migrations/20191126135206-create-help-orders.js
@@ -1,9 +1,43 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('help_orders', { id: Sequelize.INTEGER });
+    return queryInterface.createTable('help_orders', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      student_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'students', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      question: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      answer: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      answer_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
   },
 
-  down: (queryInterface, Sequelize) => {
+  down: queryInterface => {
     return queryInterface.dropTable('help_orders');
   },
 };
